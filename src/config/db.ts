c9a2e9511feef0651db9df094e0b8fd05deb294e
Zobaidul Kazi <153954612+zbkazi@users.noreply.github.com>
2024-06-06 22:16:56 +0000

    mongodb connects success

---
+++ src/config/db.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import "dotenv/config";
+
+
+// Your MongoDB connection URI. This should ideally be fetched from environment variables.
+const mongoURI = process.env.MONGODB_URL as string;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(mongoURI as string);
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Event listeners for connection errors and successful connection
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB successfully");
+});
+
+// Export the connection for reuse in other parts of your application
+export default db;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('hello')
+import app from "./app";
+import db from "./config/db";
+import "dotenv/config";
+
+app;
+db;
+
+
+// error handling middleware
+
+// 404 not found middleware
+app.use((_req, res, _next) => {
+  return res.status(404).json({
+    success: false,
+    message: "Not found",
+  });
+});
+// server error middleware
+app.use((err, _req, res, _next) => {
+  return res.status(500).json({
+    success: false,
+    message: err.message,
+  });
+});
+
+// bad request middleware
+app.use((err, _req, res, _next) => {
+  return res.status(400).json({
+    success: false,
+    message: err.message,
+  });
+});
