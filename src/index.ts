2c2b27f89c50f4efd3f53aab1d1b9b83da4b43c7
Zobaidul Kazi <153954612+zbkazi@users.noreply.github.com>
2024-06-07 15:48:53 +0000

    admin add

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
-// Create an Express application
 const app = express();
+const PORT = process.env.PORT || 3000; // Use the port specified by the environment variable or default to 3000
 
-// Define a port number
-const PORT = process.env.PORT || 4000;
-
-// Define some routes
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello, World!');
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
 
-app.get('/about', (_req: Request, res: Response) => {
-  res.send('About page');
-});
-
-app.get('/contact', (_req: Request, res: Response) => {
-  res.send('Contact page');
-});
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
