24c729a5bbeb294750c20825c13f93dba00238c6
zobkazi <zobaidulkazi64.official@gmail.com>
2024-07-02 03:17:20 +0600

    app as index row

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
+import app from "./app";
 
-const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(cors());
-app.use(morgan("dev"));
 
-app.get("/", (_req, res) => {
-  res.send("<h1>Hello Kazi Byte!, Welcome to Kazi Byte API</h1>");
-});
-
-app.listen();
 
 export default app;
